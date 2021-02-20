fbb95f768787b863d108bce54dbddef7573fb596
Bahman Baidar <bbaid001@wch136-22.cs.ucr.edu>
2021-02-19 18:50:56 -0800

    dsadsad

--- source/main.c
+++ source/main.c
@@ -26,7 +26,7 @@ unsigned char GetBit(unsigned char port, unsigned char number)
 //unsigned char KEYPADPORT;
 
 unsigned char GetKeypadKey() {
-	return '4';
+	return '6';
 /*	KEYPADPORT = PORTC;
 	unsigned char KEYPADPIN = PINC;
 	unsigned char ROW1 = 0;
